daf13f57df9d10a64df7b1cae34e624ff0e27a6f
chhak0503 <chhak0503@gmail.com>
2025-05-26 14:46:02 +0900

    add farmstory

--- farmstory-front/src/App.jsx
+++ farmstory-front/src/App.jsx
@@ -3,6 +3,14 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./styles/App.scss";
 
+/*
+npm add react-router-dom
+npm install @reduxjs/toolkit react-redux
+npm add axios
+npm install react-cookie
+npm install sass
+*/
+
 function App() {
   return (
     <div id="container">
