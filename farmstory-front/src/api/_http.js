e40d8abd61eb47060fea66b112255f1d6b5b2a60
chhak0503 <chhak0503@gmail.com>
2025-05-30 09:38:43 +0900

    add farstory

--- farmstory-front/src/api/_http.js
+++ farmstory-front/src/api/_http.js
@@ -9,3 +9,7 @@ export const USER_LOGOUT = `${SERVER_HOST}/user/logout`;
 // article
 export const ARTICLE_WRITE = `${SERVER_HOST}/article`;
 export const ARTICLE_LIST = `${SERVER_HOST}/article`;
+
+// product
+export const PRODUCT_REGISTER = `${SERVER_HOST}/product`;
+export const PRODUCT_LIST = `${SERVER_HOST}/product`;
